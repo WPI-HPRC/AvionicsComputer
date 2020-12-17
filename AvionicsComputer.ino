94a01380a2f62fe9f468a7bceb5fd5f549027b43
Christian M. Schrader <cmschrader@wpi.edu>
2020-12-17 16:00:30 -0500

    Rename

--- AvionicsComputer.ino
+++ AvionicsComputer.ino
@@ -1,13 +1,13 @@
 #include "Arduino.h"
 
-
+const uint32_t BAUD = 115200;
 
 
 
 // The setup routine runs once when you press reset:
 void setup() {
 
-	Serial.begin(9600);		// issues sometime arise if this isn't done in setup
+	Serial.begin(BAUD);		// issues sometime arise if this isn't done in setup
 
 	while(!Serial);			// loop until serial port is opened, stalls program
 
