dd23eaabc62dabd7c757ef8d0ec1a849799e9bad
Giovanni Giacalone <gbgiacalone@wpi.edu>
2020-12-23 16:58:12 -0500

    MPU6050.h and MPU6050.cpp files functional
    Basic functionality for reading raw MPU6050 Gyro and Accelerometer
    rates/values.

--- AvionicsComputer.ino
+++ AvionicsComputer.ino
@@ -1,22 +1,25 @@
 #include "Arduino.h"
-#include "MPU6050.h"
+
+#include "src/peripheral/MPU6050.h"
 
 const uint32_t BAUD = 115200;
+
+MPU6050 myMPU;
 
 void setup() {
 
 	Serial.begin(BAUD);								// issues sometime arise if this isn't done in setup
 	while(!Serial);									// loop until serial port is opened, stalls program
 
-	MPU6050->enable();									//Runs the gyroStart method that
+	myMPU.enable();									//Runs the gyroStart method that
 
 	Serial.println(F("Setup ran"));					//Prints the statement "Setup ran" to indicate that the program is working correctly.
 }
 
 void loop() {
 
-	getGyroValues();
-	Serial.println(acc_z);
-	delay(5);
+	myMPU.update();
+	Serial.println(myMPU.getAcc_z());
+	delay(1000);
 
 }
