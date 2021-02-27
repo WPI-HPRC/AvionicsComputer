61f440cfb41dea6266995b4977ce8a2e1276d24f
Max Schrader <cmschrader@wpi.edu>
2021-02-27 17:39:19 -0500

    New Airbrake Header

---
+++ src/AirbrakeController.h
@@ -0,0 +1,15 @@
+/*
+ * AirbrakeController.h
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: Christian M. Schrader
+ */
+
+#ifndef SRC_AIRBRAKECONTROLLER_H_
+#define SRC_AIRBRAKECONTROLLER_H_
+
+
+
+
+
+#endif /* SRC_AIRBRAKECONTROLLER_H_ */
