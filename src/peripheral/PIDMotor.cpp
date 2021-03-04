69b49fa8e496069af94062aa620f365724cf21bd
Abby Hyde <abbyghyde@gmail.com>
2021-03-04 17:21:36 -0500

    added PIDMotor source and header files

---
+++ src/peripheral/PIDMotor.cpp
@@ -0,0 +1,30 @@
+/*
+ * PIDMotor.cpp
+ *
+ *  Created on: Mar 4, 2021
+ *      Author: Abby Hyde
+ */
+
+#include "PIDMotor.h"
+
+PIDMotor::PIDMotor() {
+
+}
+
+void PIDMotor::enable() {
+
+
+}
+
+void PIDMotor::disable() {
+
+
+}
+
+void PIDMotor::update() {
+
+
+}
+
+
+
---
+++ src/peripheral/PIDMotor.h
@@ -0,0 +1,28 @@
+/*
+ * PIDMotor.h
+ *
+ *  Created on: Mar 4, 2021
+ *      Author: Abby Hyde
+ */
+
+#ifndef SRC_PERIPHERAL_PIDMOTOR_H_
+#define SRC_PERIPHERAL_PIDMOTOR_H_
+
+#include "Arduino.h"
+#include "Encoder.h"
+#include "PeripheralInterface.h"
+
+class PIDMotor : public PeripheralInterface {
+public:
+    PIDMotor();
+
+    void enable();
+    void disable();
+    void update();
+
+};
+
+
+
+
+#endif /* SRC_PERIPHERAL_PIDMOTOR_H_ */
